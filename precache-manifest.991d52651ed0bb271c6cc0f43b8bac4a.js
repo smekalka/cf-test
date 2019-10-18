110329c5541ccf11ef9b69d6cc7cfd1b4a89c939
davletbaev <inferion00@gmail.com>
2019-10-18 15:36:15 +0300

    Updates

---
+++ precache-manifest.991d52651ed0bb271c6cc0f43b8bac4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852a2fb41664f4ba82be3fb78efa8fe5",
+    "url": "/cf-test/index.html"
+  },
+  {
+    "revision": "c4080e4fea9234886c9f",
+    "url": "/cf-test/static/css/main.72b1c5c6.chunk.css"
+  },
+  {
+    "revision": "d9064a7da473fec308aa",
+    "url": "/cf-test/static/js/2.9073d286.chunk.js"
+  },
+  {
+    "revision": "c4080e4fea9234886c9f",
+    "url": "/cf-test/static/js/main.c0219659.chunk.js"
+  },
+  {
+    "revision": "38dd4d898b7297347f6b",
+    "url": "/cf-test/static/js/runtime-main.0e3a09ff.js"
+  }
+]);
